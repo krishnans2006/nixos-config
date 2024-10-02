a0f596d2cc60f440b949ec975cdd6b60c1fdca3a
Krishnan Shankar <krishnans2006@gmail.com>
2024-10-02 16:33:38 -0500

    Add Vivaldi (temp: to get old settings/passwords from)

--- home.nix
+++ home.nix
@@ -53,6 +53,13 @@
     })
 
     spotify
+
+    # temp
+    (vivaldi.overrideAttrs (oldAttrs: {
+      dontWrapQtApps = false;
+      dontPatchELF = true;
+      nativeBuildInputs = oldAttrs.nativeBuildInputs ++ [ pkgs.kdePackages.wrapQtAppsHook ];
+    }))
   ];
 
   # basic configuration of git, please change to your own
