e863f210bc97d16e0f608d6e531af00f79f1d905
Krishnan Shankar <krishnans2006@gmail.com>
2024-11-24 22:37:52 -0600

    Add comma (through nix-index)

--- flake.nix
+++ flake.nix
@@ -15,10 +15,15 @@
       inputs.home-manager.follows = "home-manager";
     };
 
+    nix-index-database = {
+        url = "github:nix-community/nix-index-database";
+        inputs.nixpkgs.follows = "nixpkgs";
+    };
+
     nur.url = "github:nix-community/NUR";
   };
 
-  outputs = { self, nixpkgs, home-manager, plasma-manager, nur, ... }@inputs: {
+  outputs = { self, nixpkgs, home-manager, plasma-manager, nix-index-database, nur, ... }@inputs: {
     nixosConfigurations.krishnan-lap = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
       modules = [
@@ -31,6 +36,11 @@
           home-manager.useUserPackages = true;
           home-manager.sharedModules = [ plasma-manager.homeManagerModules.plasma-manager ];
           home-manager.users.krishnan.imports = [ ./home.nix ./krishnan-lap-home.nix ];
+        }
+
+        nix-index-database.nixosModules.nix-index
+        {
+          programs.nix-index-database.comma.enable = true;
         }
 
         nur.nixosModules.nur
@@ -48,6 +58,11 @@
           home-manager.useUserPackages = true;
           home-manager.sharedModules = [ plasma-manager.homeManagerModules.plasma-manager ];
           home-manager.users.krishnan.imports = [ ./home.nix ./krishnan-pc-home.nix ];
+        }
+
+        nix-index-database.nixosModules.nix-index
+        {
+          programs.nix-index-database.comma.enable = true;
         }
 
         nur.nixosModules.nur
