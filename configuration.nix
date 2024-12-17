28b2cf3b2c3233a8174ba8ffab948767a3bf7927
Krishnan Shankar <krishnans2006@gmail.com>
2024-12-17 15:16:46 -0600

    Add Steam

--- configuration.nix
+++ configuration.nix
@@ -175,6 +175,13 @@
 
   services.tailscale.enable = true;
 
+  programs.steam = {
+    enable = true;
+    remotePlay.openFirewall = true;  # Open ports in the firewall for Steam Remote Play
+    dedicatedServer.openFirewall = true;  # Open ports in the firewall for Source Dedicated Server
+    localNetworkGameTransfers.openFirewall = true;  # Open ports in the firewall for Steam Local Network Game Transfers
+  };
+
   # List services that you want to enable:
 
   # Enable the OpenSSH daemon.
