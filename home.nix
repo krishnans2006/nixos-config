019b3df41a4c66836cc1a91baf7791f1edb777bc
Krishnan Shankar <krishnans2006@gmail.com>
2025-07-19 18:38:53 -0400

    Modularize basic home-manager options

--- home.nix
+++ home.nix
@@ -10,13 +10,11 @@ let
   }) { inherit (pkgs) system; config.allowUnfree = true; };
 in {
   imports = [
+    ./config/basic-home.nix
     ./config/secrets-home.nix
 
     ./modules/plasma.nix
   ];
-
-  home.username = "krishnan";
-  home.homeDirectory = "/home/krishnan";
 
   modules.plasma.enable = true;
 
@@ -519,17 +517,4 @@ in {
   };
 
   programs.vscode.enable = true;
-
-  # This value determines the home Manager release that your
-  # configuration is compatible with. This helps avoid breakage
-  # when a new home Manager release introduces backwards
-  # incompatible changes.
-  #
-  # You can update home Manager without changing this value. See
-  # the home Manager release notes for a list of state version
-  # changes in each release.
-  home.stateVersion = "23.11";
-
-  # Let home Manager install and manage itself.
-  programs.home-manager.enable = true;
 }
