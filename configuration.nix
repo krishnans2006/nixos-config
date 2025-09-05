6308e00fa43e21c4db5eb97e18f6cc41ab750e43
Krishnan Shankar <krishnans2006@gmail.com>
2025-09-05 11:05:48 -0500

    Fix VirtualBox

--- configuration.nix
+++ configuration.nix
@@ -125,6 +125,8 @@
 
   # VirtualBox
   virtualisation.virtualbox.host.enable = true;
+  virtualisation.virtualbox.host.enableExtensionPack = true;
+  boot.kernelParams = [ "kvm.enable_virt_at_load=0" ];
   users.extraGroups.vboxusers.members = [ "krishnan" ];
 
   # Shell
