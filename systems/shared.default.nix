56362c52a1b2097fcc728b98ad32d05c92b0a411
Krishnan Shankar <krishnans2006@gmail.com>
2025-09-19 21:36:59 -0500

    Use symlinks to remove default.nix duplication

---
+++ systems/shared.default.nix
@@ -0,0 +1,33 @@
+# Note: This file is shared (symlinked) into multiple system directories (e.g. krishnan-lap, krishnan-pc).
+# Any changes made here will affect all systems that use this shared configuration.
+# Be very careful!
+
+{ inputs, ... }:
+
+with inputs;
+
+nixpkgs.lib.nixosSystem {
+  modules = [
+    ./configuration.nix
+
+    lanzaboote.nixosModules.lanzaboote
+
+    sops-nix.nixosModules.sops
+
+    home-manager.nixosModules.home-manager
+    {
+      home-manager.useGlobalPkgs = true;
+      home-manager.useUserPackages = true;
+      home-manager.sharedModules = [ sops-nix.homeManagerModules.sops plasma-manager.homeModules.plasma-manager ];
+      home-manager.users.krishnan.imports = [
+        ./home.nix
+        nix-flatpak.homeManagerModules.nix-flatpak
+      ];
+    }
+
+    nix-index-database.nixosModules.nix-index
+    {
+      programs.nix-index-database.comma.enable = true;
+    }
+  ];
+}
