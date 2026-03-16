ab4e69e43600182f4d13f8e9883425c7f677ed4d
Krishnan Shankar <krishnans2006@gmail.com>
2026-03-16 01:42:58 -0500

    WIP Fix secrets decryption when using impermanence
    We have to use the keys in /persist instead of directly in /home, since the bind mounts are created *after* sops tries to decrypt secrets.

--- config/system/secrets.nix
+++ config/system/secrets.nix
@@ -1,8 +1,17 @@
-{ config, pkgs, ... }:
+{ config, pkgs, lib, ... }:
 
+with lib;
+
+let
+  # Sops runs sops-install-secrets before impermanence creates a bind mount
+  # So when sops runs under impermanence, /home/krishnan/.config/sops/age/keys.txt
+  # will not be available, even if persisted in environment.persistence
+  # Therefore we have to pull the key from /persist directly
+  useImpermanence = config.environment.persistence ? "/persist";
+in
 {
   sops = {
-    age.keyFile = "/home/krishnan/.config/sops/age/keys.txt";
+    age.keyFile = "${optionalString useImpermanence "/persist"}/home/krishnan/.config/sops/age/keys.txt";
     age.generateKey = false;  # Do it manually from an SSH key (see README.md)
     defaultSopsFile = ../../secrets/system.yaml;
     defaultSopsFormat = "yaml";
