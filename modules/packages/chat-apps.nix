32828a75e1dcfa6b3641ea3aa2f06bd9b1950841
Krishnan Shankar <krishnans2006@gmail.com>
2026-04-28 09:59:03 -0500

    Add autostart config for Element, Mattermost, Zulip
    Also, improve config for Vesktop and Slack.

--- modules/packages/chat-apps.nix
+++ modules/packages/chat-apps.nix
@@ -28,13 +28,23 @@ in
         # These variables read an app's desktop file from /nix/store
         # then modify the Exec= value to start the app minimized
         vesktopOld = builtins.readFile "${pkgs.vesktop}/share/applications/vesktop.desktop";
-        vesktopNew = builtins.replaceStrings [ "vesktop %U" ] [ "vesktop -m %U" ] vesktopOld;
+        vesktopNew = builtins.replaceStrings [ "vesktop %U" ] [ "vesktop --start-minimized %U" ] vesktopOld;
 
         slackOld = builtins.readFile "${pkgs.slack}/share/applications/slack.desktop";
-        slackNew = builtins.replaceStrings [ "slack -s %U" ] [ "slack -s -u %U" ] slackOld;
+        slackNew = builtins.replaceStrings [ "slack -s %U" ] [ "slack -s --startup %U" ] slackOld;
+
+        elementOld = builtins.readFile "${pkgs.element-desktop}/share/applications/element-desktop.desktop";
+        elementNew = builtins.replaceStrings [ "element-desktop %u" ] [ "element-desktop --hidden %u" ] elementOld;
+
+        mattermost = builtins.readFile "${pkgs.mattermost-desktop}/share/applications/Mattermost.desktop";
+
+        zulip = builtins.readFile "${pkgs.zulip}/share/applications/zulip.desktop";
       in {
         "autostart/vesktop.desktop".text = vesktopNew;
         "autostart/slack.desktop".text = slackNew;
+        "autostart/element.desktop".text = elementNew;
+        "autostart/mattermost.desktop".text = mattermost;
+        "autostart/zulip.desktop".text = zulip;
       }
     );
   };
