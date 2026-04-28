8b54df980c3b080873d82196c56caa4a096da6bf
Krishnan Shankar <krishnans2006@gmail.com>
2026-04-28 17:00:08 -0500

    Override lutris-free to fix failing test for openldap

--- base/home.nix
+++ base/home.nix
@@ -70,7 +70,24 @@
 
     lc3tools
 
-    lutris-free
+    (pkgs.lutris-free.override {
+      # Override the underlying lutris package
+      lutris = pkgs.lutris.override {
+        # Intercept buildFHSEnv to modify target packages
+        buildFHSEnv = args: pkgs.buildFHSEnv (args // {
+          multiPkgs = envPkgs:
+            let
+              # Fetch original package list
+              originalPkgs = args.multiPkgs envPkgs;
+
+              # Disable tests for openldap
+              customLdap = envPkgs.openldap.overrideAttrs (_: { doCheck = false; });
+            in
+            # Replace broken openldap with the custom one
+            builtins.filter (p: (p.pname or "") != "openldap") originalPkgs ++ [ customLdap ];
+        });
+      };
+    })
     prismlauncher  # minecraft
     #sauerbraten
 
