ada5dd58b41b534005ec56ffe73ced1d533fba32
Krishnan Shankar <krishnans2006@gmail.com>
2026-05-24 18:58:54 -0400

    Attempt to fix filesystem mounts

--- modules/system/fs-mounts.nix
+++ modules/system/fs-mounts.nix
@@ -1,11 +1,25 @@
-{ pkgs, config, lib, ... }:
+{ pkgs, config, lib, root, ... }:
 
 with lib;
 
 let
   cfg = config.modules.fs-mounts;
+  sshfsOptions = [
+    "-o" "noatime"
+    "-o" "allow_other"
+    "-o" "reconnect"
+    "-o" "ServerAliveInterval=15"
+    "-o" "ServerAliveCountMax=3"
+    "-o" "IdentityFile=${config.sops.secrets."fs-key/private".path}"
+    "-o" "StrictHostKeyChecking=no"
+    "-o" "UserKnownHostsFile=/dev/null"
+  ];
 in
 {
+  imports = [
+    "${root}/config/system/secrets.nix"
+  ];
+
   options.modules.fs-mounts = {
     tjcsl = mkEnableOption "Enable systemd mounts for TJ CSL filesystem";
     ews = mkEnableOption "Enable systemd mounts for UIUC EWS filesystem";
@@ -16,39 +30,57 @@ in
       sshfs
     ];
 
+    programs.fuse.userAllowOther = true;
+
     users.groups.fuse = { };
     users.users."krishnan".extraGroups = [ "fuse" ];
 
-    systemd.mounts = lib.optionals cfg.tjcsl [
-      {
-        what = "2024kshankar@ras2.tjhsst.edu:/csl/users/2024kshankar";
-        where = "/home/krishnan/Filesystems/tjCSL";
-        type = "fuse.sshfs";
+    systemd.services = mkMerge [
+      (mkIf cfg.tjcsl {
+        systemd.services."mount-tjcsl" = {
+          description = "SSHFS mount for TJ CSL filesystem";
+          wantedBy = [ "multi-user.target" ];
+          wants = [ "network-online.target" ];
+          after = [ "network-online.target" ];
 
-        options = lib.concatStringsSep "," [ "_netdev" "noatime" "IdentityFile=/home/krishnan/.ssh/id_ed25519" ];
-      }
-    ] ++ lib.optionals cfg.ews [
-      {
-        what = "ks128@linux.ews.illinois.edu:/home/ks128";
-        where = "/home/krishnan/Filesystems/EWS";
-        type = "fuse.sshfs";
+          serviceConfig = {
+            Type = "simple";
+            ExecStart = lib.concatStringsSep " " ([
+              "${pkgs.sshfs}/bin/sshfs"
+              "2024kshankar@ras2.tjhsst.edu:/csl/users/2024kshankar"
+              "/home/krishnan/Filesystems/tjCSL"
+              "-f"
+            ] ++ sshfsOptions);
+            ExecStop = "/run/wrappers/bin/fusermount -u /home/krishnan/Filesystems/tjCSL";
+            Restart = "on-failure";
+            RestartSec = "5";
+            ExecStartPre = "${pkgs.coreutils}/bin/mkdir -p /home/krishnan/Filesystems/tjCSL";
+          };
+        };
+      })
 
-        options = lib.concatStringsSep "," [ "_netdev" "noatime" "IdentityFile=/home/krishnan/.ssh/id_ed25519" ];
-      }
-    ];
+      (mkIf cfg.ews {
+        systemd.services."mount-ews" = {
+          description = "SSHFS mount for UIUC EWS filesystem";
+          wantedBy = [ "multi-user.target" ];
+          wants = [ "network-online.target" ];
+          after = [ "network-online.target" ];
 
-    systemd.automounts = lib.optionals cfg.tjcsl [
-      {
-        wantedBy = [ "multi-user.target" ];
-        where = "/home/krishnan/Filesystems/tjCSL";
-        automountConfig.TimeoutIdleSec = "30m";
-      }
-    ] ++ lib.optionals cfg.ews [
-      {
-        wantedBy = [ "multi-user.target" ];
-        where = "/home/krishnan/Filesystems/EWS";
-        automountConfig.TimeoutIdleSec = "30m";
-      }
+          serviceConfig = {
+            Type = "simple";
+            ExecStart = lib.concatStringsSep " " ([
+              "${pkgs.sshfs}/bin/sshfs"
+              "ks128@linux.ews.illinois.edu:/home/ks128"
+              "/home/krishnan/Filesystems/EWS"
+              "-f"
+            ] ++ sshfsOptions);
+            ExecStop = "/run/wrappers/bin/fusermount -u /home/krishnan/Filesystems/EWS";
+            Restart = "on-failure";
+            RestartSec = "5";
+            ExecStartPre = "${pkgs.coreutils}/bin/mkdir -p /home/krishnan/Filesystems/EWS";
+          };
+        };
+      })
     ];
   };
 }
