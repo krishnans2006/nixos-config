2e6cd460fbd6bf182d375424c47ea5ce9f1df5b9
Krishnan Shankar <krishnans2006@gmail.com>
2026-06-29 12:52:34 -0400

    Add some declarative Zulip config
    This does not include domain.json.

--- modules/packages/zulip.nix
+++ modules/packages/zulip.nix
@@ -4,6 +4,8 @@ with lib;
 
 let
   cfg = config.modules.packages.zulip;
+
+  formatter = pkgs.formats.json { };
 in
 {
   options.modules.packages.zulip = {
@@ -13,6 +15,41 @@ in
 
   config = mkIf cfg.enable {
     home.packages = with pkgs; [ zulip ];
+
+    # Declarative config
+    xdg.configFile."Zulip/config/settings.json".source = (
+      formatter.generate "settings.json" {
+        appLanguage = "en";
+        enableSpellchecker = true;
+        spellcheckerLanguages = null;
+        autoHideMenubar = true;
+        silent = false;
+        startMinimized = true;
+        trayIcon = true;
+        useSystemProxy = false;
+        useManualProxy = false;
+        autoUpdate = true;
+        showSidebar = true;
+        badgeOption = true;
+        startAtLogin = true;  # Maybe false?
+        showNotification = true;
+        betaUpdate = false;
+        errorReporting = false;
+        customCSS = false;
+        lastActiveTab = 0;
+        dnd = false;
+        dndPreviousSettings = {
+          showNotification = true;
+          silent = false;
+        };
+        downloadsPath = "${config.home.homeDirectory}/Downloads/Zulip";
+        quitOnClose = false;
+        promptDownload = false;
+        proxyPAC = "";
+        proxyRules = "";
+        proxyBypass = "";
+      }
+    );
 
     # Impermanence
     modules.impermanence.persistDirs = [
