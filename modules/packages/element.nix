6809dc118890a84fbc48a4cb204a4ddaa41574e6
Krishnan Shankar <krishnans2006@gmail.com>
2026-06-29 11:23:36 -0400

    Use programs.element-desktop for declarative Element config

--- modules/packages/element.nix
+++ modules/packages/element.nix
@@ -12,7 +12,39 @@ in
   };
 
   config = mkIf cfg.enable {
-    home.packages = with pkgs; [ element-desktop ];
+    programs.element-desktop = {
+      enable = true;
+      settings = {
+        brand = "Element";
+        default_theme = "dark";
+
+        default_server_config = {
+          "m.homeserver" = {
+            base_url = "https://matrix-client.matrix.org";
+            server_name = "matrix.org";
+          };
+          "m.identity_server" = {
+            base_url = "https://vector.im";
+          };
+        };
+        disable_custom_urls = true;  # Force matrix.org for now
+
+        force_verification = true;  # Must verify new logins
+        disable_login_language_selector = true;  # Hide language selector
+        disable_3pid_login = true;  # Disable email/phone login
+        disable_guests = false;
+
+        # https://web-docs.element.dev/config.html#integration-managers
+        integrations_ui_url = "https://scalar.vector.im/";
+        integrations_rest_url = "https://scalar.vector.im/api";
+        integrations_widgets_urls = [
+          "https://scalar.vector.im/_matrix/integrations/v1"
+          "https://scalar.vector.im/api"
+          "https://scalar-staging.vector.im/_matrix/integrations/v1"
+          "https://scalar-staging.vector.im/api"
+        ];
+      };
+    };
 
     # Impermanence
     modules.impermanence.persistDirs = [
