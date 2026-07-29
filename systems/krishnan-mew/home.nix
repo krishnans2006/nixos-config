8c44d31d11b3e5f931883b2f31d1328ae2675970
Krishnan Shankar <krishnans2006@gmail.com>
2026-07-28 21:29:44 -0400

    Add basic home.nix for krishnan-mew

---
+++ systems/krishnan-mew/home.nix
@@ -0,0 +1,61 @@
+{ inputs, root, ... }:
+
+with inputs;
+
+{
+  imports = [
+    # Base configuration
+    "${root}/base/home.nix"
+
+    # Custom modules
+    (import-tree "${root}/modules/home")
+    (import-tree "${root}/modules/packages")
+  ];
+
+  modules.impermanence.enable = false;
+  modules.secrets.enable = mkForce false;
+
+  modules.plasma.enable = false;
+  modules.tailscale.enable = true;
+  modules.shell = {
+    enable = true;
+    enableDotfiles = true;
+    enableTheme = true;
+    enableAtuin = true;
+    enableZoxide = true;
+  };
+  modules.direnv.enable = true;
+  modules.git.enable = true;
+  modules.ssh.enable = true;
+  modules.fs-mounts = {
+    tjcsl = false;
+    ews = false;
+  };
+  modules.gaming = {
+    enableLutris = false;
+    enableMinecraft = false;
+  };
+
+  modules.packages = {
+    utils.enable = true;
+    vesktop.enable = false;
+    vesktop.autostart = false;
+    slack.enable = false;
+    slack.autostart = false;
+    element.enable = false;
+    element.autostart = false;
+    mattermost.enable = false;
+    mattermost.autostart = false;
+    zulip.enable = false;
+    zulip.autostart = false;
+    zen-browser.enable = false;
+    zen-browser.autostart = false;
+    bitwarden-desktop.enable = false;
+    bitwarden-desktop.autostart = false;
+    firefox.enable = false;
+    thunderbird.enable = false;
+    libreoffice.enable = false;
+    jetbrains.enableAll = false;
+    zed-editor.enable = false;
+  };
+}
