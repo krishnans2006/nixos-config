712596dfd9f93fdcf3ef57b102f7894c36560f1a
Krishnan Shankar <krishnans2006@gmail.com>
2026-07-28 20:28:27 -0400

    Add basic system.nix for krishnan-mew

---
+++ systems/krishnan-mew/system.nix
@@ -0,0 +1,59 @@
+{ inputs, root, ... }:
+
+with inputs;
+
+{
+  imports = [
+    # Include the results of the hardware scan.
+    ./hardware.nix
+
+    # Disk configuration (disko)
+    ./disk.nix
+
+    # Base configuration
+    "${root}/base/system.nix"
+
+    # Custom modules
+    (import-tree "${root}/modules/system")
+  ];
+
+  modules.impermanence.enable = false;
+
+  modules.plasma.enable = false;
+  modules.audio.enable = false;
+  modules.networks = {
+    enable = false;
+  };
+  modules.bluetooth.enable = false;
+  modules.printing.enable = false;
+  modules.docker.enable = false;
+  modules.tailscale = {
+    enable = true;
+    enableTaildrive = false;
+  };
+  modules.localsend.enable = false;
+  modules.ssh-server.enable = false;
+  modules.secure-boot.enable = false;
+  modules.krishnan-user.enable = false;  # TODO without secrets
+  modules.iphone.enable = false;
+
+  modules.gaming.enable = false;
+  modules.waydroid.enable = false;
+  modules.virtualbox.enable = false;
+  modules.vmware.enable = false;
+
+  modules.asusd.enable = false;
+  modules.hp-pen.enable = false;
+  modules.yubikey-auth.enable = false;
+
+  modules.packages = {
+    logic2 = false;
+    chipwhisperer = false;
+  };
+
+  # Custom config
+
+  networking.hostName = "krishnan-mew";  # Define your hostname.
+
+  time.timeZone = "America/Chicago";
+}
