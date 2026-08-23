e04a58c2a4d201670b387dd198f6bb5d7c0157a2
Krishnan Shankar <krishnans2006@gmail.com>
2026-08-23 17:04:48 -0500

    Switch ronix to new url (GitHub)

--- flake.nix
+++ flake.nix
@@ -72,7 +72,7 @@
 
     vscode-server.url = "github:nix-community/nixos-vscode-server";
 
-    ronix.url = "git+https://codeberg.org/caniko/ronix";
+    ronix.url = "github:caniko/ronix";
   };
 
   # nixConfig = {
