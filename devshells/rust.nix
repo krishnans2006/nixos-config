00f31a3ee697e9de592e245f5d5ff7f8b02f5ddc
Krishnan Shankar <krishnans2006@gmail.com>
2026-09-02 02:45:22 -0500

    Add rust devshell

---
+++ devshells/rust.nix
@@ -0,0 +1,59 @@
+# flake.nix
+{
+  description = "A Nix flake for Distributed Systems";
+
+  inputs = {
+    fenix = {
+      url = "github:nix-community/fenix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    nixpkgs.url = "nixpkgs/nixos-unstable";
+  };
+
+  outputs = inputs:
+  let
+    inherit (inputs.nixpkgs) lib;
+
+    supportedSystems = [
+      "x86_64-linux"
+      "aarch64-linux"
+      "aarch64-darwin"
+    ];
+    forEachSupportedSystem = f: lib.genAttrs supportedSystems (
+      system: f {
+        pkgs = import inputs.nixpkgs {
+          inherit system;
+          overlays = [ inputs.self.overlays.default ];
+        };
+      }
+    );
+  in
+  {
+    overlays.default = final: prev: {
+      rustToolchain =
+        with inputs.fenix.packages.${prev.stdenv.hostPlatform.system};
+        combine (with stable; [ clippy rustc cargo rustfmt rust-src ]);
+    };
+
+    devShells = forEachSupportedSystem (
+      { pkgs }: {
+        default = pkgs.mkShell {
+          packages = with pkgs; [
+            rustToolchain
+            openssl
+            pkg-config
+            cargo-deny
+            cargo-edit
+            cargo-watch
+            rust-analyzer
+          ];
+
+          env = {
+            # Required by rust-analyzer
+            RUST_SRC_PATH = "${pkgs.rustToolchain}/lib/rustlib/src/rust/library";
+          };
+        };
+      }
+    );
+  };
+}
