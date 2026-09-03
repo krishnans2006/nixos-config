krishnans2006/nixos-config

commit ccf792037e36d9d1df0f040d773117e750a9b9a9

Only show battery in tray if the system has one

modules/home/plasma.nix
@@ -9,7 +9,11 @@ in
 {
   options.modules.plasma = {
     enable = mkEnableOption "Enable a customized KDE Plasma 6 DE";
-    #
+    hasBattery = mkOption {
+      type = types.bool;
+      default = true;
+      description = "Whether the system has a battery";
+    };
   };
 
   # Must be enabled in system config
@@ -338,14 +342,9 @@ in
                   items = {
                     showAll = false;
                     shown = [
-                      "org.kde.plasma.battery"
                       "org.kde.plasma.bluetooth"
-                    ];
-                    hidden = [
-                      "org.kde.plasma.addons.katesessions"
-                      #"org.kde.plasma.diskquota"
-                      #"org.kde.plasma.weatherreport"
-                    ];
+                    ] ++ optional (cfg.hasBattery) [ "org.kde.plasma.battery" ];
+                    hidden = [ "org.kde.plasma.addons.katesessions" ];
                   };
                 };
               }

systems/krishnan-lap/home.nix
@@ -15,7 +15,10 @@ with inputs;
   modules.secrets.enable = true;
   modules.nix-user-chroot.enable = false;
 
-  modules.plasma.enable = true;
+  modules.plasma = {
+    enable = true;
+    hasBattery = false;
+  };
   modules.fonts.enable = true;
   modules.tailscale.enable = true;
   modules.shell = {

systems/krishnan-pc/home.nix
@@ -15,7 +15,10 @@ with inputs;
   modules.secrets.enable = true;
   modules.nix-user-chroot.enable = false;
 
-  modules.plasma.enable = true;
+  modules.plasma = {
+    enable = true;
+    hasBattery = false;
+  };
   modules.fonts.enable = true;
   modules.tailscale.enable = true;
   modules.shell = {

systems/krishnan-vivo/home.nix
@@ -15,7 +15,10 @@ with inputs;
   modules.secrets.enable = true;
   modules.nix-user-chroot.enable = false;
 
-  modules.plasma.enable = true;
+  modules.plasma = {
+    enable = true;
+    hasBattery = true;
+  };
   modules.fonts.enable = true;
   modules.tailscale.enable = true;
   modules.shell = {

commit 25409d5e5135f3d824c07ea3f17ea4e642267b99

Hotfix: Fix build failure caused by ccf792037e36d9d1df0f040d773117e750a9b9a9

modules/home/plasma.nix
@@ -343,7 +343,7 @@ in
                     showAll = false;
                     shown = [
                       "org.kde.plasma.bluetooth"
-                    ] ++ optional (cfg.hasBattery) [ "org.kde.plasma.battery" ];
+                    ] ++ optional (cfg.hasBattery) "org.kde.plasma.battery";
                     hidden = [ "org.kde.plasma.addons.katesessions" ];
                   };
                 };