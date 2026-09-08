1044673bb34f472da9735f60580133a0751473bc
Krishnan Shankar <krishnans2006@gmail.com>
2026-09-08 13:34:42 -0500

    Improve code style
    Don't use unnecessary `inputs.` prefixes.

--- systems/krishnan-ews/default.nix
+++ systems/krishnan-ews/default.nix
@@ -3,20 +3,20 @@
 with inputs;
 
 home-manager.lib.homeManagerConfiguration {
-  pkgs = inputs.nixpkgs.legacyPackages.x86_64-linux;
+  pkgs = nixpkgs.legacyPackages.x86_64-linux;
 
   extraSpecialArgs = {
     inherit (inputs) import-tree;
-    root = inputs.self;
+    root = self;
   };
 
   modules = [
     # These are needed since they provide options used in modules/home
     # I don't think they can be easily removed, but I'm not sure if this is
     # even a concern (do they even take up storage space in the Nix store?)
-    inputs.sops-nix.homeManagerModules.sops
-    inputs.plasma-manager.homeModules.plasma-manager
-    inputs.nix-flatpak.homeManagerModules.nix-flatpak
+    sops-nix.homeManagerModules.sops
+    plasma-manager.homeModules.plasma-manager
+    nix-flatpak.homeManagerModules.nix-flatpak
     ./home.nix
   ];
 }
