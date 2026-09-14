64b8f93713a3c8a1156e8e65f0d9115177e88efa
Krishnan Shankar <krishnans2006@gmail.com>
2026-09-13 21:08:36 -0500

    Fix ~/Filesystems permissions when Taildrive is enabled
    Since Taildrive runs after tailscaled, when on an impermanence system, it creates ~/Filesystems as root. Then, when
    fs-mounts runs as a user service, it fails due to not being able to create the directory:
    
    mkdir: cannot create directory ‘/home/krishnan/Filesystems/tjCSL’: Permission denied
    
    This fixes the problem by using systemd.tmpfiles to create the directories owned by a user if necessary.

--- modules/system/capabilities/tailscale.nix
+++ modules/system/capabilities/tailscale.nix
@@ -12,7 +12,7 @@ in
     enableTaildrive = mkEnableOption "Enable Tailscale Taildrive";
     taildrivePath = mkOption {
       type = types.str;
-      default = "home/krishnan/Filesystems/Tailscale";
+      default = "/home/krishnan/Filesystems/Tailscale";
       description = "Path to mount Taildrive";
     };
   };
@@ -117,47 +117,73 @@ in
       }
     ))
 
-    (mkIf cfg.enableTaildrive {
-      services.davfs2 = {
-        enable = true;
-        davGroup = "davfs2";
-      };
-      users.users."krishnan".extraGroups = [ "davfs2" ];
+    (mkIf cfg.enableTaildrive (
+      let
+        homeUserMatch = builtins.match "^/home/([^/]+)(/.*)?$" cfg.taildrivePath;
+        homeUser = if homeUserMatch != null then builtins.head homeUserMatch else null;
+        parentDir = dirOf cfg.taildrivePath;
+      in
+      {
+        services.davfs2 = {
+          enable = true;
+          davGroup = "davfs2";
+        };
+        users.users."krishnan".extraGroups = [ "davfs2" ];
 
-      systemd.mounts = [
-        {
-          # what = "http://100.100.100.100:8080/krishnans2006%%40gmail.com";  # %40 = @
-          what = "http://100.100.100.100:8080";
-          where = "${cfg.taildrivePath}";
-          type = "davfs";
-
-          wants = [ "tailscaled.service" ];
-          after = [ "tailscaled.service" ];
-
-          options = lib.concatStringsSep "," [
-            "noatime"
-            "_netdev"
-            "file_mode=0664"
-            "dir_mode=2775"
-            "user"
-            "uid=${toString config.users.users."krishnan".uid}"
-            "grpid"
+        systemd.tmpfiles.settings = mkIf (homeUser != null) {
+          "10-taildrive" = mkMerge [
+            (mkIf (parentDir != "/home") {
+              "${parentDir}".d = {
+                mode = "0755";
+                user = homeUser;
+                group = "users";
+              };
+            })
+            {
+              "${cfg.taildrivePath}".d = {
+                mode = "0755";
+                user = homeUser;
+                group = "users";
+              };
+            }
           ];
-        }
-      ];
-      systemd.automounts = [
-        {
-          wantedBy = [ "multi-user.target" ];
-          where = "${cfg.taildrivePath}";
-          automountConfig.TimeoutIdleSec = "30m";
-        }
-      ];
+        };
 
-      environment.etc."davfs2/secrets" = {
-        enable = true;
-        text = "http://100.100.100.100:8080 \"\" \"\"";
-        mode = "0600";
-      };
-    })
+        systemd.mounts = [
+          {
+            # what = "http://100.100.100.100:8080/krishnans2006%%40gmail.com";  # %40 = @
+            what = "http://100.100.100.100:8080";
+            where = "${cfg.taildrivePath}";
+            type = "davfs";
+
+            wants = [ "tailscaled.service" ];
+            after = [ "tailscaled.service" ];
+
+            options = lib.concatStringsSep "," [
+              "noatime"
+              "_netdev"
+              "file_mode=0664"
+              "dir_mode=2775"
+              "user"
+              "uid=${toString config.users.users."krishnan".uid}"
+              "grpid"
+            ];
+          }
+        ];
+        systemd.automounts = [
+          {
+            wantedBy = [ "multi-user.target" ];
+            where = "${cfg.taildrivePath}";
+            automountConfig.TimeoutIdleSec = "30m";
+          }
+        ];
+
+        environment.etc."davfs2/secrets" = {
+          enable = true;
+          text = "http://100.100.100.100:8080 \"\" \"\"";
+          mode = "0600";
+        };
+      }
+    ))
   ]);
 }
